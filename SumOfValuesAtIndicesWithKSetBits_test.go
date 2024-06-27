44cf0c953894f2b9ed44fddf4344f30bdc34c309
Dmitrii Kliuev <dmitrii.kliuev.1989@gmail.com>
2024-06-27 20:44:12 +0300

    wip

---
+++ SumOfValuesAtIndicesWithKSetBits_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSumOfValuesAtIndicesWithKSetBits_test(t *testing.T) {
+	nums := []int{5, 10, 1, 5, 2}
+	k := 1
+	expected := 13
+
+	actual := sumIndicesWithKSetBits(nums, k)
+
+	assert.Equal(t, expected, actual)
+}
+
+func sumIndicesWithKSetBits(nums []int, k int) int {
+	sum := 0
+
+	for i, num := range nums {
+		count := 0
+		tmp := i
+
+		for j := 0; j < 16; j++ {
+			count += tmp & 1
+			tmp >>= 1
+		}
+
+		if count == k {
+			sum += num
+		}
+	}
+
+	return sum
+}
